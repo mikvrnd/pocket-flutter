c10ce95a0b69e34089e3392091b81acd5345f9eb
mikvrnd <mikarnd2@gmail.com>
2021-07-16 12:07:54 +0200

    quick fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:pocket_app/home.dart';
 import 'package:pocket_app/view/welcome/welcome.dart';
 import 'constants.dart';
 import 'common.dart';
